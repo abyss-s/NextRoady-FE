ba9040160fd66c9c350f1b00c493cffb665b2d3e
abyss-s <xyz@inha.edu>
2024-10-02 16:34:37 +0900

    setting: add font-family

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-4xl font-pb underline">Hello world!</h1>
+      <p class="font-pr">이것은 Pretendard Regular 폰트입니다.</p>
+      <p class="font-pl">이것은 Pretendard Light 폰트입니다.</p>
+      <p class="font-pm">이것은 Pretendard Medium 폰트입니다.</p>
+      <p class="font-ps">이것은 Pretendard SemiBold 폰트입니다.</p>
+      <p class="font-pb">이것은 Pretendard Bold 폰트입니다.</p>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    colors: {},
+
+    fontFamily: {
+      pr: ["Pretendard-Regular"],
+      pl: ["Pretendard-Light"],
+      pm: ["Pretendard-Medium"],
+      ps: ["Pretendard-SemiBold"],
+      pb: ["Pretendard-Bold"],
+    },
+
     extend: {},
   },
   plugins: [],
-}
+};
+
