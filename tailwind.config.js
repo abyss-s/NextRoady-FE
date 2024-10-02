41e427ebd6a0a7b6d6c362b92d2c4d25fcba6095
abyss-s <xyz@inha.edu>
2024-10-02 15:42:03 +0900

    setting: refactor Tailwind CSS and Vite config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
 export default {
-  content: ["./src/**/*.{html,js,jsx,mjs,cjs}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [
-    tailwindcss,
-    autoprefixer,
-  ],
+  plugins: [],
 }
