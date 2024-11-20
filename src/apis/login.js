b9e4d3d37d5fead058ec08481555ef16f952c6cb
abyss-s <xyz@inha.edu>
2024-11-21 03:49:24 +0900

    ♻️ Refactor: 로그인 api 비동기 통신 제거
    - html로 바로 리다이렉트하도록 변경

--- src/apis/login.js
+++ src/apis/login.js
@@ -3,44 +3,26 @@ import auth from './auth.js';
 const REACT = import.meta.env.VITE_REACT_DOMAIN_PORT;
 const SERVER = import.meta.env.VITE_SERVER_DOMAIN_PORT;
 
-export const loginKakao = async () => {
+export const loginKakao = () => {
   const redirectUri = `${REACT}/redirect`;
   const url = `${SERVER}/oauth2/authorization/kakao?redirect_uri=${encodeURIComponent(
     redirectUri
   )}&mode=login`;
-  try {
-    const response = await auth.get(url);
-    window.open(url, '_self');
-    return response.data;
-  } catch (error) {
-    console.error('Kakao login error: ', error);
-  }
+  window.location.href = url;
 };
 
-export const loginNaver = async () => {
+export const loginNaver = () => {
   const redirectUri = `${REACT}/redirect`;
   const url = `${SERVER}/oauth2/authorization/naver?redirect_uri=${encodeURIComponent(
     redirectUri
   )}&mode=login`;
-  try {
-    const response = await auth.get(url); // API 호출
-    window.open(url, '_self');
-    return response.data;
-  } catch (error) {
-    console.error('Naver login error: ', error);
-  }
+  window.location.href = url;
 };
 
-export const loginGoogle = async () => {
+export const loginGoogle = () => {
   const redirectUri = `${REACT}/redirect`;
   const url = `${SERVER}/oauth2/authorization/google?redirect_uri=${encodeURIComponent(
     redirectUri
   )}&mode=login`;
-  try {
-    const response = await auth.get(url);
-    window.open(url, '_self');
-    return response.data;
-  } catch (error) {
-    console.error('Google login error: ', error);
-  }
+  window.location.href = url;
 };
