b822a0d1a70313f6257d06efb204abe73e8a7fdc
abyss-s <xyz@inha.edu>
2024-11-21 21:45:21 +0900

    ♻️ Refactor: 마크다운 렌더러 수정

--- src/components/MarkdownRenderer/MarkdownRenderer.jsx
+++ src/components/MarkdownRenderer/MarkdownRenderer.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
+// import remarkGfm from 'remark-gfm';
 
-const MarkdownRenderer = ({ content }) => {
+const MarkdownRenderer = ({ markdown }) => {
   return (
-    <div className="prose">
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>
-        {content}
+    <div>
+      <ReactMarkdown>
+        {/* <ReactMarkdown remarkPlugins={[remarkGfm]}> */}
+        {markdown}
       </ReactMarkdown>
     </div>
   );
