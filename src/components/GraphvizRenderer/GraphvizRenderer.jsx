add2e9a069a605d8749ade51cf9a7accc0cad5f1
abyss-s <xyz@inha.edu>
2024-11-21 17:28:11 +0900

    🐛 Fix: 그래프비즈 렌더러에 그래프 내장하도록 수정

--- src/components/GraphvizRenderer/GraphvizRenderer.jsx
+++ src/components/GraphvizRenderer/GraphvizRenderer.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 import Graphviz from 'graphviz-react';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 
-const GraphvizRenderer = ({ dot }) => {
+const Graph = ({ dot }) => {
   useEffect(() => {}, [dot]);
 
   return (
@@ -11,6 +12,21 @@ const GraphvizRenderer = ({ dot }) => {
         options={{ useWorker: false, width: 1200, height: 800 }}
       />
     </div>
+  );
+};
+
+const GraphvizRenderer = ({ dot }) => {
+  return (
+    <TransformWrapper
+      initialScale={1}
+      minScale={0.7}
+      maxScale={5}
+      zoomAnimation
+    >
+      <TransformComponent>
+        <Graph dot={dot} />
+      </TransformComponent>
+    </TransformWrapper>
   );
 };
 
